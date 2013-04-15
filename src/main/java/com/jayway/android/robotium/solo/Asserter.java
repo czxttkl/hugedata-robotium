6b88ce851f6cc4e93aa314d32be7e256822ab2d8
czxttkl <czxttkl@gmail.com>
2013-04-15 23:51:24 +0800

    我改过的Robotium

---
+++ src/main/java/com/jayway/android/robotium/solo/Asserter.java
@@ -0,0 +1,162 @@
+package com.jayway.android.robotium.solo;
+
+import junit.framework.Assert;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Contains assert methods examples are assertActivity() and assertLowMemory().
+ * 
+ * @author Renas Reda, renasreda@gmail.com
+ *
+ */
+
+class Asserter {
+	private final ActivityUtils activityUtils;
+	private final Waiter waiter;
+
+	/**
+	 * Constructs this object.
+	 *
+	 * @param activityUtils the {@code ActivityUtils} instance.
+     * @param waiter the {@code Waiter} instance.
+	 */
+
+	public Asserter(ActivityUtils activityUtils, Waiter waiter) {
+		this.activityUtils = activityUtils;
+		this.waiter = waiter;
+    }
+
+	/**
+     * Asserts that an expected {@link Activity} is currently active one.
+     *
+     * @param message the message that should be displayed if the assert fails
+     * @param name the name of the {@code Activity} that is expected to be active e.g. {@code "MyActivity"}
+     */
+
+	public void assertCurrentActivity(String message, String name)
+	{
+		Assert.assertTrue(message, waiter.waitForActivity(name));
+	}
+	
+	/**
+     * Asserts that an expected {@link Activity} is currently active one.
+     *
+     * @param message the message that should be displayed if the assert fails
+     * @param name the name of the {@code Activity} that is expected to be active e.g. {@code "MyActivity"}
+     * @param timeout the timeout for wait
+     */
+	public void assertCurrentActivity(String message, String name, int timeout)
+	{
+		Assert.assertTrue(message, waiter.waitForActivity(name, timeout));
+	}
+	
+	/**
+     * Asserts that an expected {@link Activity} is currently active one.
+     * If failed, the phone will take screenshots automatically.
+     *
+     * @param message the message that should be displayed if the assert fails
+     * @param name the name of the {@code Activity} that is expected to be active e.g. {@code "MyActivity"}
+     * @param timeout the timeout for wait
+     * @param solo Solo instance
+     */
+	public void assertCurrentActivity(String message, String name, int timeout, Solo solo)
+	{
+		if(!waiter.waitForActivity(name, timeout)){
+			Log.i("Robotium",name + " failed");
+			solo.takeScreenshot("test_" + message + "_Failure");
+		    Assert.assertTrue(message, false);
+		}else {
+			Log.i("Robotium",name + " success");
+			solo.takeScreenshot("test_" + message + "_Success");
+		}
+	}
+
+	/**
+     * Asserts that an expected {@link Activity} is currently active one.
+     *
+     * @param message the message that should be displayed if the assert fails
+	 * @param expectedClass the {@code Class} object that is expected to be active e.g. {@code MyActivity.class}
+	 */
+
+	public void assertCurrentActivity(String message, Class<? extends Activity> expectedClass)
+	{
+		Assert.assertTrue(message, waiter.waitForActivity(expectedClass.getSimpleName()));
+	}
+	
+	/**
+	 * Asserts that an expected {@link Activity} is currently active one, with the possibility to
+	 * verify that the expected {@code Activity} is a new instance of the {@code Activity}.
+	 * 
+	 * @param message the message that should be displayed if the assert fails
+	 * @param name the name of the {@code Activity} that is expected to be active e.g. {@code "MyActivity"}
+	 * @param isNewInstance {@code true} if the expected {@code Activity} is a new instance of the {@code Activity}
+	 */
+	
+	public void assertCurrentActivity(String message, String name, boolean isNewInstance)
+	{
+		assertCurrentActivity(message, name);
+		assertCurrentActivity(message, activityUtils.getCurrentActivity().getClass(),
+				isNewInstance);
+	}
+	
+	/**
+	 * Asserts that an expected {@link Activity} is currently active one, with the possibility to
+	 * verify that the expected {@code Activity} is a new instance of the {@code Activity}.
+	 * 
+	 * @param message the message that should be displayed if the assert fails
+	 * @param expectedClass the {@code Class} object that is expected to be active e.g. {@code MyActivity.class}
+	 * @param isNewInstance {@code true} if the expected {@code Activity} is a new instance of the {@code Activity}
+	 */
+	
+	public void assertCurrentActivity(String message, Class<? extends Activity> expectedClass,
+			boolean isNewInstance) {
+		boolean found = false;
+		assertCurrentActivity(message, expectedClass);
+		Activity activity = activityUtils.getCurrentActivity(false);
+		for (int i = 0; i < activityUtils.getAllOpenedActivities().size() - 1; i++) {
+			String instanceString = activityUtils.getAllOpenedActivities().get(i).toString();
+			if (instanceString.equals(activity.toString()))
+				found = true;
+		}
+			Assert.assertNotSame(message + ", isNewInstance: actual and ", isNewInstance, found);
+	}
+	
+	/**
+	 * Asserts that the available memory is not considered low by the system.
+	 */
+
+	public void assertMemoryNotLow()
+	{
+		ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+		((ActivityManager)activityUtils.getCurrentActivity().getSystemService("activity")).getMemoryInfo(mi);
+		Assert.assertFalse("Low memory available: " + mi.availMem + " bytes", mi.lowMemory);
+	}
+
+	/**
+	 * Asserts that the specified view is shown or visible.
+	 */
+
+	public void assertViewShown(String message, View view, int timeout)
+	{
+		Assert.assertTrue(message, waiter.isViewShown(view, timeout));
+	}
+	
+	/**
+	 * Asserts that the specified view is shown or visible and take screenshots.
+	 */
+
+	public void assertViewShown(String message, View view, int timeout, Solo solo)
+	{
+		if(!waiter.isViewShown(view, timeout)){
+			solo.takeScreenshot("test_" + message + "_Failure");
+		    Assert.assertTrue(message, false);
+		}else {
+			solo.takeScreenshot("test_" + message + "_Success");
+		}
+	}
+	
+	
+}
